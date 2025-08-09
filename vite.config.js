eb1e342d207fcfbea258fe95b9f7a59a68f8f11d
ryuuzoh-hayashi <ryuuzoh-hayashi@SM2406-0025.lan>
2025-08-09 17:25:01 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ryuzo-hayashi.github.io/triangle-area-react/' // ★ リポジトリ名に合わせて変更
+  base: '/triangle-area-react/'   // ★ フルURLではなく先頭/末尾スラッシュ付きのパス
 })
